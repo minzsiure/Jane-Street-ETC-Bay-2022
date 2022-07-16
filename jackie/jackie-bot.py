cede7db4169b3a39e3871f5469cbae4f02fd67fa
Jackie Lin <jackiel@mit.edu>
2022-07-16 12:25:20 -0700

    fix

--- jackie/jackie-bot.py
+++ jackie/jackie-bot.py
@@ -223,11 +223,11 @@ class ExchangeConnection:
         """Send an order with maximum size possible based on existing limits and current orders."""
         if dir == Dir.BUY:
             buy_limit = limits[symbol] - positions[symbol] - pending_positions[symbol]["buy"]
-            print("BUYING WITH SIZE", symbol, buy_limit, positions[symbol], pending_positions[symbol])
+            print("BUY", symbol, buy_limit, positions[symbol], pending_positions[symbol])
             self.send_add_message(symbol, dir, price, buy_limit)
         else: 
             sell_limit = limits[symbol] + positions[symbol] - pending_positions[symbol]["sell"]
-            print("BUYING WITH SIZE", symbol, buy_limit, positions[symbol], pending_positions[symbol])
+            print("SELL", symbol, sell_limit, positions[symbol], pending_positions[symbol])
             self.send_add_message(symbol, dir, price, sell_limit)
 
 
