6352f9c6455a42fbbe884f54a23fd2630c600f6d
Jackie Lin <jackiel@mit.edu>
2022-07-16 15:48:44 -0700

    removed arbitrage

--- bot.py
+++ bot.py
@@ -123,14 +123,15 @@ def main():
 
                 if message_type == "convert":
                     if symbol == "VALE":
-                        exchange.send_limit_add_message(symbol="VALE", dir=Dir.SELL, price=bid_price["VALE"] - 5)
+                        # exchange.send_limit_add_message(symbol="VALE", dir=Dir.SELL, price=bid_price["VALE"] - 5)
+                        pass
                 else:
                     if symbol == "BOND":
                         exchange.send_limit_add_message(symbol="BOND", dir=Dir.SELL, price=1001)
-                    if symbol == "VALE":
-                        exchange.send_limit_convert_message(symbol="VALE", dir=Dir.SELL, size=size)
-                    if symbol == "VALBZ":
-                        exchange.send_limit_convert_message(symbol="VALE", dir=Dir.BUY, size=size)
+                    # if symbol == "VALE":
+                    #     exchange.send_limit_convert_message(symbol="VALE", dir=Dir.SELL, size=size)
+                    # if symbol == "VALBZ":
+                    #     exchange.send_limit_convert_message(symbol="VALE", dir=Dir.BUY, size=size)
                 
             else:
                 positions[symbol] -= size
@@ -138,13 +139,14 @@ def main():
 
                 if message_type == "convert":
                     if symbol == "VALE":
-                        exchange.send_limit_add_message(symbol="VALBZ", dir=Dir.SELL, price=bid_price["VALBZ"] - 5)
+                        # exchange.send_limit_add_message(symbol="VALBZ", dir=Dir.SELL, price=bid_price["VALBZ"] - 5)
+                        pass
                 else:
                     if symbol == "BOND":
                         exchange.send_limit_add_message(symbol="BOND", dir=Dir.BUY, price=999)
 
         elif message["type"] == "book":
-            update_fair_value(exchange, message)
+            # update_fair_value(exchange, message)
 
             # Always run arbitrage buying engine. 
             vale_valbz_arbitrage(exchange=exchange)
