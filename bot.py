2abff05fc30acf50ef784d45757ac0caf0e886a0
Amy Hu <amyhu@mit.edu>
2022-07-16 14:22:19 -0700

    undo bot changes

--- bot.py
+++ bot.py
@@ -132,8 +132,8 @@ def main():
                     exchange.send_limit_add_message(symbol="BOND", dir=Dir.BUY, price=999)
 
         elif message["type"] == "book":
-            update_fair_value(exchange, message)
-
+            #update_fair_value(exchange, message)
+            i = 0
             # Always run arbitrage buying engine. 
             # vale_valbz_arbitrage(exchange=exchange)
 
