d7c8fc3b33137311a503d3ffb473daa6342f86b5
minzsiure <xieyi@mit.edu>
2022-07-16 15:17:40 -0700

    .

--- eva/eva-test.py
+++ eva/eva-test.py
@@ -177,9 +177,9 @@ def check_and_buy_arbitrage_XLF_amount(exchange, positions, category, amount_to_
     if category == "XLF":
         XLF_pos = positions["XLF"]
         # not enough, buy more xLf
-        print("trying to buy XLF, condition:", XLF_pos - amount_to_match["XLF"] < 0 and ask_price["XLF"] <= 0.95*fair_value["XLF"])
+        print("trying to buy XLF, condition:", XLF_pos - amount_to_match["XLF"] < 0)
         print("We have", XLF_pos, "many XLF, and current ask is", ask_price["XLF"], "comparing .95 fair is",0.95*fair_value["XLF"])
-        if XLF_pos - amount_to_match["XLF"] < 0 and ask_price["XLF"] <= 0.95*fair_value["XLF"]:
+        if XLF_pos - amount_to_match["XLF"] < 0:
             exchange.send_add_message(symbol="XLF", dir=Dir.BUY, price=round(ask_price["XLF"]), size=amount_to_match["XLF"]-XLF_pos) 
             print("checked. not enough XLF", "buying", amount_to_match["XLF"]-XLF_pos, "at", fair_value["XLF"])
             return True
