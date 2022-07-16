e20e79d0593e401e44f5140c3b192d5a79e568c9
Jackie Lin <jackiel@mit.edu>
2022-07-16 15:43:56 -0700

    delete ar

--- bot.py
+++ bot.py
@@ -123,14 +123,15 @@ def main():
 
                 if message_type == "convert":
                     if symbol == "VALE":
-                        exchange.send_limit_add_message(symbol="VALE", dir=Dir.SELL, price=bid_price["VALE"] - 5)
+                        # exchange.send_limit_add_message(symbol="VALE", dir=Dir.SELL, price=bid_price["VALE"] - 5)
+                        pass
                 else:
                     if symbol == "BOND":
                         exchange.send_limit_add_message(symbol="BOND", dir=Dir.SELL, price=1001)
-                    if symbol == "VALE":
-                        exchange.send_limit_convert_message(symbol="VALE", dir=Dir.SELL, size=size)
-                    if symbol == "VALBZ":
-                        exchange.send_limit_convert_message(symbol="VALE", dir=Dir.BUY, size=size)
+                    # if symbol == "VALE":
+                    #     exchange.send_limit_convert_message(symbol="VALE", dir=Dir.SELL, size=size)
+                    # if symbol == "VALBZ":
+                    #     exchange.send_limit_convert_message(symbol="VALE", dir=Dir.BUY, size=size)
                 
             else:
                 positions[symbol] -= size
@@ -138,7 +139,8 @@ def main():
 
                 if message_type == "convert":
                     if symbol == "VALE":
-                        exchange.send_limit_add_message(symbol="VALBZ", dir=Dir.SELL, price=bid_price["VALBZ"] - 5)
+                        # exchange.send_limit_add_message(symbol="VALBZ", dir=Dir.SELL, price=bid_price["VALBZ"] - 5)
+                        pass
                 else:
                     if symbol == "BOND":
                         exchange.send_limit_add_message(symbol="BOND", dir=Dir.BUY, price=999)
@@ -147,7 +149,7 @@ def main():
             update_fair_value(exchange, message)
 
             # Always run arbitrage buying engine. 
-            vale_valbz_arbitrage(exchange=exchange)
+            # vale_valbz_arbitrage(exchange=exchange)
 
 
 def update_fair_value(exchange, message):
