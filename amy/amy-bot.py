165b50eda613cc8c6d2039bda04ae1e99f2add93
Amy Hu <amyhu@mit.edu>
2022-07-16 16:29:29 -0700

    m

--- amy/amy-bot.py
+++ amy/amy-bot.py
@@ -185,7 +185,7 @@ def update_fair_value(exchange, message):
         ask_price[symbol] = message["sell"][0][0]
     if symbol in {"VALBZ", "GS", "MS", "WFC"}:
         if bid_price[symbol] and ask_price[symbol]:
-            cur_price = (bid_price[symbol] + ask_price[symbol]) / 2
+            cur_price = (bid_price[symbol] * message["buy"][0][1] + ask_price[symbol] * message["sell"][0][1]) / (message["buy"][0][1] + message["sell"][0][1])
         elif bid_price[symbol]:
             cur_price = bid_price[symbol]
         elif ask_price[symbol]:
